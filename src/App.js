858d1d899b340c5d7cb6cbabff2745c8b48fbd13
Abdul Hanan Sharif <146536242+hanansharif@users.noreply.github.com>
2024-01-01 10:55:01 +0500

    settup app for Redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Shop from './Shop';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello World</h2>
-    </div>
+    <>
+      <Navbar />
+      <div className="container my-3">
+        <Shop />
+      </div>
+    </>
   );
 }
 
