198eae7efbfa5a45d41ddfafdd87ac5f9e2464f7
Abdul Hanan Sharif <146536242+hanansharif@users.noreply.github.com>
2024-01-01 11:52:27 +0500

    creating reducers and action creators and settup redux

--- src/Shop.js
+++ src/Shop.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Shop = () => {
     return (
         <div>
-            <h2>Buy Adidas Shoes 50 Rs</h2>
+            <h2>Deposit/Wiithdraw Money</h2>
             <button className="btn btn-primary mx-2"> - </button>
-            Add to Cart
+            Update Balance
             <button className="btn btn-primary mx-2"> + </button>
         </div>
     )
