cbc377ba42ba926fd35f1f40fce93559dae4f1be
Abdul Hanan Sharif <146536242+hanansharif@users.noreply.github.com>
2024-01-03 10:10:12 +0500

    removal of deprecated libraries and adding new ones

--- src/state/store.js
+++ src/state/store.js
@@ -1,6 +1,17 @@
-import { applyMiddleware, createStore } from 'redux';
-import reducers from './reducers';
-import { thunk } from 'redux-thunk';
+// import { applyMiddleware, createStore } from 'redux';
+// import reducers from './reducers';
+// import { thunk } from 'redux-thunk';
 
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk))
+// export const store = createStore(reducers, {}, applyMiddleware(thunk))
+
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
+
+export const store = configureStore({
+    reducer: rootReducer,
+    // Other configuration options if needed
+});
+
+// export default store;
+
